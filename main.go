a775d0d590fe3e58e45e6767f8e1176f6ac14e7d
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-18 23:15:27 -0700

    added newCard function

--- main.go
+++ main.go
@@ -5,8 +5,14 @@ import "fmt"
 func main() {
 	// var card string = "Ace of Spades"
 	//BOTH of these are the same
-	card := "Ace of Spades"
+	// card := "Ace of Spades"
 	//ONLY use ":=" when Initializing the value (card = "NEW VALUE")
 
+	card := newCard()
+
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
