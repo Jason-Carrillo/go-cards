46a50bd856cd2e82b6e47eae0ad416cbc07aeb44
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-19 11:59:01 -0700

    added more checks to check first, and last card in deck

--- deck_test.go
+++ deck_test.go
@@ -7,6 +7,14 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Errorf("Expected deck length of 20, but got " + len(d))
+		t.Errorf("Expected deck length of 20, but got %d", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of Spades but got ", d[0])
+	}
+
+	if d[d.len-1] != "Four of Cpades" {
+		t.Errorf("Expected last card to be Four of Clubs but got ", d[d.len-1])
 	}
 }
