087610cac2b210f43b0ea81a834dc40c69b9bfa8
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-19 11:48:12 -0700

    added eroro print line

--- deck_test.go
+++ deck_test.go
@@ -1,1 +1,12 @@
 package main
+
+import "testing"
+
+// TEST function are all first character UPPER case NOT standard camelCase
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 20, but got " + len(d))
+	}
+}
