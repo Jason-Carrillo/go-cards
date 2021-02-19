ea0d2007689576cb42e41ea7059831c5a88f5f41
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-18 23:03:31 -0700

    added comment to clarify

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades"
+	//BOTH of these are the same
 	card := "Ace of Spades"
+	//ONLY use ":=" when Initializing the value (card = "NEW VALUE")
 
 	fmt.Println(card)
 }
