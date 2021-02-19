012d19960de1872a5585b08c55b3f8d9ac9a7eea
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-19 09:53:24 -0700

    added loop of suits, and values

--- deck.go
+++ deck.go
@@ -7,6 +7,22 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hears", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for i, suit := range cardSuits {
+		for j, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+// Typically calling the reference a shortened version of the receiver d = deck
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
